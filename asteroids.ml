270ba29f49247d51152e435c59e73dbac3cf7794
dark-rabbit <darkrabbitlolagyar@gmail.com>
2015-03-04 03:03:33 +0100

    creation des types, etat initial et affichage du vaisseau

--- asteroids.ml
+++ asteroids.ml
@@ -6,19 +6,90 @@ open Graphics;;
 (* dimension fenetre graphique *)
 let width = 1000;;
 let height = 600;;
-
+let pi = 3.14;;
 
 (* --- definition types pour etat du jeu --- *)
 
+type spaceship = {
+
+    x : float; y : float;   (* les coordonnees du centre de rotation du vaisseau *)
+    angle : float;          (* l'angle du vaisseau en radiants *)
+    speed : float * float;  (* la vitesse pour deplacements et anticipation des collisions *)
+
+    (* on utilise des float pour les calculs, on convertit en int pour l'affichage, cf ci-dessous *)
+
+    (* les points (calcules en fonction de center et rotation) du triangle qu'on va dessiner avec draw_poly *)
+    vertices : (int * int) array
+};;
+
+(* fonction de calcul des vertices, ca trace un triangle avec l'appel de draw_poly sur vertices *)
+(* dimensions (10 et 20) a revoir ! *)
+let refreshShipVertices spaceship =
+    let x = spaceship.x
+    and y = spaceship.y
+    and a = spaceship.angle
+    and v = spaceship.vertices in
+    v.(0) <- (int_of_float(x +. 20. *. cos(a)), int_of_float(y +. 20. *. sin(a)));
+    v.(1) <- (int_of_float(x +. 10. *. cos(a -. pi/.2.)), int_of_float(y +. 10. *. sin(a -. pi/.2.)));
+    v.(2) <- (int_of_float(x +. 10. *. cos(a +. pi/.2.)), int_of_float(y +. 10. *. sin(a +. pi/.2.)));;
+
+(* pour les lasers, on fait des petits cercles ou des segments ? *)
+type laser = {
+    vertices : (int * int) array;
+    speed : int * int
+}
+
+type asteroidCategory = Big | Medium | Small;;
+
+type asteroid = {
+
+    (* la dimension et le comportement de l'asteroide en cas de collision va changer suivant sa categorie *)
+    category : asteroidCategory;
+
+    (*
+     * les trois parametres de la fonction draw_circle
+     * dans le jeu original, les asteroides sont des polygones generes aleatoirement
+     * donc a voir
+     *)
+    x : int;
+    y : int;
+    r : int;
+
+    speed : int * int
+};;
+
 (* A DEFINIR : positions, deplacements, etc. *)
-  
-type etat = unit;; (* A REDEFINIR *)
+
+type etat = {
+
+    (*
+     * Un etat comporte le joueur,
+     * ainsi que deux listes dynamiques pour les asteroides et les lasers
+     *)
+    player : spaceship;
+    lasers : laser list;
+    asteroids : asteroid list
+};;
 
 (* --- initialisations etat --- *)
 
 (* A DEFINIR : generation positions, deplacements initiaux ... *)
-  
-let init_etat () = ();; (* A REDEFINIR *)
+
+let player0 = {
+    x = float_of_int (width / 2);
+    y = float_of_int (height / 2);
+    angle = pi /. 2.;
+    speed = (0., 0.);
+    vertices = Array.make 3 (0,0)
+};;
+refreshShipVertices player0;;
+
+let init_etat () = 
+    let etat = {
+        player = player0;
+        lasers = [];
+        asteroids = []
+} in etat;;
 
 (* --- changements d'etat --- *)
 
@@ -39,31 +110,31 @@ let etat_suivant etat = etat;; (* A REDEFINIR *)
 (* --- affichages graphiques --- *)
 
 (* fonctions d'affichage du vaisseau, d'un asteroide, etc. *)
-    
-let affiche_etat etat = ();; (* A REDEFINIR *)
 
+let affiche_etat etat = 
+    draw_poly etat.player.vertices;; (* A REDEFINIR *)
 
 (* --- boucle d'interaction --- *)
 
 let rec boucle_interaction ref_etat =
-  let status = wait_next_event [Key_pressed] in (* on attend une frappe clavier *)
-  let etat = !ref_etat in (* on recupere l'etat courant *)
-  let nouvel_etat = (* on definit le nouvel etat... *)
-    match status.key with (* ...en fonction de la touche frappee *)
+    let status = wait_next_event [Key_pressed] in (* on attend une frappe clavier *)
+    let etat = !ref_etat in (* on recupere l'etat courant *)
+    let nouvel_etat = (* on definit le nouvel etat... *)
+        match status.key with (* ...en fonction de la touche frappee *)
     | '1' | 'j' -> rotation_gauche etat (* rotation vers la gauche *)
     | '2' | 'k' -> acceleration etat (* acceleration vers l'avant *)
     | '3' | 'l' -> rotation_droite etat (* rotation vers la droite *)
     | ' ' -> tir etat (* tir d'un projectile *)
     | 'q' -> print_endline "Bye bye!"; exit 0 (* on quitte le jeux *)
     | _ -> etat in (* sinon, rien ne se passe *)
-  ref_etat := nouvel_etat; (* on enregistre le nouvel etat *)
+    ref_etat := nouvel_etat; (* on enregistre le nouvel etat *)
   boucle_interaction ref_etat;; (* on se remet en attente de frappe clavier *)
 
 (* --- fonction principale --- *)
-    
+
 let main () =
-  (* initialisation du generateur aleatoire *)
-  Random.self_init ();
+    (* initialisation du generateur aleatoire *)
+    Random.self_init ();
   (* initialisation de la fenetre graphique et de l'affichage *)
   open_graph (" " ^ string_of_int width ^ "x" ^ string_of_int height);
   auto_synchronize false;
@@ -75,7 +146,7 @@ let main () =
       Unix.it_value = 0.05 } in
   Sys.set_signal Sys.sigalrm
     (Sys.Signal_handle (fun _ ->
-      affiche_etat !ref_etat; (* ...afficher l'etat courant... *)
+        affiche_etat !ref_etat; (* ...afficher l'etat courant... *)
       synchronize ();
       ref_etat := etat_suivant !ref_etat)); (* ...puis calculer l'etat suivant *)
   boucle_interaction ref_etat;; (* lancer la boucle d'interaction avec le joueur *)
